d829614948199430ea1f10932dff7130c43ff5b2
panda916 <67279281+panda916@users.noreply.github.com>
2024-01-08 15:04:17 +0700

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,35 +7,36 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
 
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-
-chrome_options = Options()
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--window-size=1920x1080")
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--log-level=3')
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.by import By
+#from selenium.common.exceptions import NoSuchElementException
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
+#import time
+#
+#from selenium.webdriver.chrome.service import Service as ChromeService
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#chrome_options = webdriver.ChromeOptions()
+##chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--no-sandbox')
+#chrome_options.add_argument('--disable-dev-shm-usage')
+#
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#
+#
+#chrome_options = Options()
+#chrome_options.add_argument("--incognito")
+#chrome_options.add_argument("--window-size=1920x1080")
+#options = webdriver.ChromeOptions()
+#options.add_argument('--headless')
+#options.add_argument('--disable-gpu')
+#options.add_argument('--log-level=3')
 
 import os, sys
 import argparse
