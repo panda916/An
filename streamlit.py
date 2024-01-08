173172b8d8765e369ac9c2ad421de915d93ebbbd
panda916 <67279281+panda916@users.noreply.github.com>
2024-01-08 15:45:30 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,49 +6,20 @@
 
 import streamlit as st
 import streamlit.components.v1 as stc
+
+
+
+
+#import libraries and set up environment
+import numpy as np
 import pandas as pd
-
-#from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
-#from selenium.webdriver.common.by import By
-#from selenium.common.exceptions import NoSuchElementException
-#from webdriver_manager.chrome import ChromeDriverManager
-#from selenium.webdriver.chrome.service import Service
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.common.by import By
-#import time
-#
-#from selenium.webdriver.chrome.service import Service as ChromeService
-#from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
-#chrome_options = webdriver.ChromeOptions()
-##chrome_options.add_argument('--headless')
-#chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--disable-dev-shm-usage')
-#
-#from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
-#
-#
-#chrome_options = Options()
-#chrome_options.add_argument("--incognito")
-#chrome_options.add_argument("--window-size=1920x1080")
-#options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
-#options.add_argument('--log-level=3')
-
-import os, sys
-import argparse
-
-
-
-# In[25]:
-
-
-# File Processing Pkgs
-import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import IsolationForest
+import pyodbc
+from sklearn import tree
+from pandas.api.types import is_numeric_dtype
+import time
+import pickle
 
 
 def main():
@@ -67,9 +38,70 @@ def main():
             file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
             st.write(file_details)
             df = pd.read_csv(data_file)
+            start = time.time()
+            B01_01_TT_LOAD_FROM_CSV = df
+            end = time.time()
+            print(end-start)
+            B01_01_TT_LOAD_FROM_CSV.shape
             st.dataframe(df)
+            
+            # filter out XVORL = X 
+            B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV[B01_01_TT_LOAD_FROM_CSV['XVORL'] != 'X'] # ok
+            # filter out ZBNKN = NULL
+            B01_01_TT_LOAD_FROM_CSV = B01_01_TT_LOAD_FROM_CSV.dropna(subset=['ZBNKN']) # ZBNKN is not null 
 
+            B01_01_TT_LOAD_FROM_CSV.shape
+            
+            ZF_COLUMNS = ['ZF_KUNNR_LIFNR_EMPFG','ZNME1','ZBNKN','RBETR','ZLAND','ZBNKS']
+            B01_02_TT_TRAIN_MODEL = B01_01_TT_LOAD_FROM_CSV[ZF_COLUMNS]
+            
+            B01_02_TT_TRAIN_MODEL = B01_02_TT_TRAIN_MODEL.astype(str)
+            B01_02_TT_TRAIN_MODEL['RBETR'] = pd.to_numeric(B01_02_TT_TRAIN_MODEL['RBETR'], errors='coerce')
+            
+            #Convert category to numberic value.
+            start = time.time()
+            ZF_LABEL_ENCODER = LabelEncoder()
+            for col_name in B01_02_TT_TRAIN_MODEL.columns:
+                if is_numeric_dtype(B01_02_TT_TRAIN_MODEL[col_name]) == True:
+                    pass
+                else:
+                    B01_02_TT_TRAIN_MODEL[col_name] = ZF_LABEL_ENCODER.fit_transform(B01_02_TT_TRAIN_MODEL[col_name])
+            end = time.time()
+            print(end-start)
+            
+            
+            # Note: You only run code in this cell and ignore the next cell if this is the first time you run isolation forest. If you have already trained isolation forest before and saved, run code in the next cell and ignore this cell.
 
+            # Create a Isolation Forest model that is called isf and training it with B01_02_TT_TRAIN_MODEL
+            # Note: n_estimators, max_samples, contamination, max_features should be adjusted to fit with different data set
+            start = time.time()
+            ZF_ISF_MODEL = IsolationForest(n_estimators = 500, max_samples = 500000, contamination = 0.001, max_features = 7)
+            ZF_ISF_MODEL.fit(B01_02_TT_TRAIN_MODEL)
+            end = time.time()
+
+            # Save the trained ZF_ISF_MODEL for next times. (you can edit model name)
+            ZF_MODEL_NAME_SAVE = 'modelname.sav'
+            pickle.dump(ZF_ISF_MODEL, open(ZF_MODEL_NAME_SAVE, 'wb'))
+            print(end-start)
+            
+            
+            # Load model which is trained before.
+            ZF_MODEL_NAME_SAVE = 'modelname.sav'
+            ZF_ISF_MODEL = pickle.load(open(ZF_MODEL_NAME_SAVE, 'rb'))
+            
+            
+            # Add result fields and Index_ field from Isolation Forest model to original data and save to SQL
+            start = time.time()
+            B01_01_TT_LOAD_FROM_CSV['Anomaly'] = pd.DataFrame(ZF_ISF_MODEL.predict(B01_02_TT_TRAIN_MODEL))
+            B01_01_TT_LOAD_FROM_CSV['Score'] = pd.DataFrame(ZF_ISF_MODEL.score_samples(B01_02_TT_TRAIN_MODEL))
+            B01_01_TT_LOAD_FROM_CSV.reset_index(inplace = True)
+            B01_01_TT_LOAD_FROM_CSV['Index_'] = B01_01_TT_LOAD_FROM_CSV['index']
+            B01_01_TT_LOAD_FROM_CSV.drop(columns = 'index', inplace = True)
+            end = time.time()
+            print(end-start)
+            
+            st.dataframe(B01_01_TT_LOAD_FROM_CSV)
+            
 # In[33]:
 
 
