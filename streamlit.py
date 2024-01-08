80585a8d286de9a32f0731db08f827da3141fb80
panda916 <67279281+panda916@users.noreply.github.com>
2024-01-08 16:44:13 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,12 +13,12 @@ import streamlit.components.v1 as stc
 #import libraries and set up environment
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing 
-from sklearn import ensemble 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import IsolationForest
+from scikit-learn import preprocessing 
+from scikit-learn import ensemble 
+from scikit-learn.preprocessing import LabelEncoder
+from scikit-learn.ensemble import IsolationForest
 import pyodbc
-from sklearn import tree
+from scikit-learn import tree
 from pandas.api.types import is_numeric_dtype
 import time
 import pickle
